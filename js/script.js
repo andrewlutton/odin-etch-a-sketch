5a2df3ff710a6821df52c283eb120e2526d58c94
alutton <andrew.lutton@gmail.com>
2022-03-14 12:32:09 -0400

    Add gridBuilder() and flex, border css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 const grid = document.querySelector(".grid-container");
 const gridTotalSize = 960; //total grid size, square, px
 
-grid.setAttribute("style",`height: ${gridTotalSize}px; width: ${gridTotalSize}px; background-color: black`);
+grid.setAttribute("style",`height: ${gridTotalSize}px; width: ${gridTotalSize}px; background-color: pink`);
 
+gridBuilder(16);
+
+function gridBuilder(size) {
+    const cellHeight = gridTotalSize / size;
+    const cellWidth = gridTotalSize / size;
+
+    for (i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.height = `${cellHeight}px`;
+        grid.appendChild(row);
+        for (j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.width = `${cellWidth}px`;
+            row.appendChild(cell);
+        }
+    }
+}
