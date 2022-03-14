4df949bf8eaf8e144647f33c3e69e9970f559668
alutton <andrew.lutton@gmail.com>
2022-03-14 14:39:39 -0400

    Add reset button functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector(".grid-container");
-const gridTotalSize = 960; //total grid size, square, px
+const GRID_TOTAL_SIZE = 480; //total grid size, square, px
+const DEFAULT_BACKGROUND_COLOR = "pink";
 
-grid.setAttribute("style",`height: ${gridTotalSize}px; width: ${gridTotalSize}px; background-color: pink`);
+grid.setAttribute("style", `height: ${GRID_TOTAL_SIZE}px; width: ${GRID_TOTAL_SIZE}px; background-color: ${DEFAULT_BACKGROUND_COLOR}`);
 
 const cells = gridBuilder(16);
 cells.forEach((cell) => {
@@ -10,22 +11,27 @@ cells.forEach((cell) => {
     });
 });
 
+const resetBtn = document.querySelector(".reset")
+resetBtn.addEventListener("click", resetGrid);
+
 function gridBuilder(size) {
-    const cellHeight = gridTotalSize / size;
-    const cellWidth = gridTotalSize / size;
 
     for (i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        // row.style.height = `${cellHeight}px`;
         grid.appendChild(row);
         for (j = 0; j < size; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
-            // cell.style.width = `${cellWidth}px`;
             row.appendChild(cell);
         }
     }
     const cells = document.querySelectorAll(".cell");
     return cells;
+}
+
+function resetGrid() {
+    cells.forEach((cell) => {
+        cell.setAttribute("style", `background-color: ${DEFAULT_BACKGROUND_COLOR}`)
+    });
 }
