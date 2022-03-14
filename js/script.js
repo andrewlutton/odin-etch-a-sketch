1e3a9a9f0aef904046dfc69a4941cb3181557e7c
alutton <andrew.lutton@gmail.com>
2022-03-14 11:52:03 -0400

    Add grid container w/ js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector(".grid-container");
+const gridTotalSize = 960; //total grid size, square, px
+
+grid.setAttribute("style",`height: ${gridTotalSize}px; width: ${gridTotalSize}px; background-color: black`);
+
