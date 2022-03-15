7f63f173afb95d13ccaf03d0062a22bac471f97a
alutton <andrew.lutton@gmail.com>
2022-03-15 09:44:22 -0400

    Fix bug with reset and change grid size

--- js/script.js
+++ js/script.js
@@ -1,37 +1,53 @@
 const grid = document.querySelector(".grid-container");
 const GRID_TOTAL_SIZE = 480; //total grid size, square, px
 const DEFAULT_BACKGROUND_COLOR = "pink";
+let cellFill = "black";
+let gridSize = 16;
+let cells;
+
+buildGrid();
+setCellFill(cellFill);
 
 grid.setAttribute("style", `height: ${GRID_TOTAL_SIZE}px; width: ${GRID_TOTAL_SIZE}px; background-color: ${DEFAULT_BACKGROUND_COLOR}`);
 
-const cells = gridBuilder(16);
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-        cell.setAttribute("style", "background-color: black");
-    });
-});
+const gridSlider = document.getElementById("grid-size");
+gridSlider.addEventListener("mouseup", changeGridSize);
 
 const resetBtn = document.querySelector(".reset")
-resetBtn.addEventListener("click", resetGrid);
+resetBtn.addEventListener("click", () => {buildGrid(); setCellFill(cellFill)});
 
-function gridBuilder(size) {
+function buildGrid() {
 
-    for (i = 0; i < size; i++) {
+    // destroy current grid
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    };
+
+    // build new grid
+    for (i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         grid.appendChild(row);
-        for (j = 0; j < size; j++) {
+        for (j = 0; j < gridSize; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
             row.appendChild(cell);
         }
     }
-    const cells = document.querySelectorAll(".cell");
-    return cells;
+    cells = document.querySelectorAll(".cell");
 }
 
-function resetGrid() {
+function changeGridSize() {
+    gridSize = this.value;
+    buildGrid(this.value);
+    setCellFill(cellFill);
+}
+
+// pass string to set cell filling mode
+function setCellFill (mode) {
     cells.forEach((cell) => {
-        cell.setAttribute("style", `background-color: ${DEFAULT_BACKGROUND_COLOR}`)
+        cell.addEventListener("mouseover", () => {
+            cell.setAttribute("style", `background-color: ${mode}`);
+        });
     });
 }
